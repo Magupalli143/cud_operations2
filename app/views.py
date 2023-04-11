9a3136f87a04131d0b65b74f061fd523093f5465
Magupalli143 <magupalliganesh@gmail.com>
2023-04-11 16:40:31 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -19,6 +19,13 @@ def display_emp(request):
     eop=emp.objects.filter(job__endswith='r')
     eop=emp.objects.filter(Ename__in=('Smith','Martin'))
     eop=emp.objects.filter(empno__contains=6)
+    eop=emp.objects.all()
+    emp.objects.filter(job='clerk').update(sal='1600')
+    eop=emp.objects.all()
+    emp.objects.update_or_create(job='MANAGER',defaults={'Ename':'gana'})
+    eop=emp.objects.all()
+    emp.objects.filter(job='MANAGER').delete()
+    eop=emp.objects.all()
 
 
     
